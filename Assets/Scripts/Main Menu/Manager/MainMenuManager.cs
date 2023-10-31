8011e7e4ab06a1a527a517d1472e1c1828625ca3
Yash Kumar Gupta <ykgupta2411@gmail.com>
2023-10-31 22:49:39 +0530

    Added main menu scene

---
+++ Assets/Scripts/Main Menu/Manager/MainMenuManager.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public GameObject loadingPanel;
+    public Image lockBase;
+    public Image lockHead;
+    public int sceneIndex = 1;
+
+    private AsyncOperation loadOperation;
+
+    private void Start()
+    {
+        loadOperation = null;
+    }
+
+    private void Update()
+    {
+        if(loadOperation == null)
+            return;
+        
+        float progress = (loadOperation.progress / 0.9f);
+        if(progress <= 0.8f)
+            lockBase.fillAmount =  progress / 0.8f;
+        else
+        {
+            lockBase.fillAmount = 1f;
+            lockHead.fillAmount = (progress - 0.8f) / 0.2f;
+        }
+
+        if(progress >= 1)
+            SetSceneActivation();
+    }
+
+    public void PlayButton()
+    {
+        loadingPanel.SetActive(true);
+        LoadScene(sceneIndex);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void LoadScene(int sceneIndex)
+    {
+        loadOperation = SceneManager.LoadSceneAsync(sceneIndex);
+        loadOperation.allowSceneActivation = false;
+    }
+
+    public void SetSceneActivation()
+    {
+        loadOperation.allowSceneActivation = true;
+    }
+}
