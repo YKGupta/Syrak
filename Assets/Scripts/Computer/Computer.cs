ead6795f6e0ad9da45b520c821057dc6a696b05a
Yash Kumar Gupta <ykgupta2411@gmail.com>
2023-11-05 22:33:55 +0530

    Made computer to trigger via 'E' button

--- Assets/Scripts/Computer/Computer.cs
+++ Assets/Scripts/Computer/Computer.cs
@@ -4,42 +4,76 @@ public class Computer : MonoBehaviour
 {
     public TriggerEvents triggerEvents;
     public DoorFunctions doorFunctions;
+    public KeyCode computerInteractionKey;
+    public GameObject InteractionInstructionsGO;
 
     private Door door;
     private bool prevDoorQuestionState;
+    private bool isTriggered;
 
     private void Start()
     {
         triggerEvents.onTriggerEnter += OnPlayerEnter;
-        doorFunctions.doorInteractionOver += OnDoorInteractionStart;
+        triggerEvents.onTriggerStay += OnPlayerStay;
+        triggerEvents.onTriggerExit += OnPlayerExit;
+        doorFunctions.doorInteractionStart += OnDoorInteractionStart;
         doorFunctions.doorInteractionOver += OnDoorInteractionOver;
+        isTriggered = false;
     }
 
     public void OnPlayerEnter(Collider playerC)
     {
         if(door == null || !playerC.CompareTag("Player"))
             return;
-        
-        prevDoorQuestionState = door.doorQuestion.showQuestion;
+
+        isTriggered = true;
+        InteractionInstructionsGO.SetActive(true);
+    }
+
+    public void OnPlayerStay(Collider playerC)
+    {
+        if(door == null || !playerC.CompareTag("Player"))
+            return;
+
+        if(!Input.GetKeyDown(computerInteractionKey))
+            return;
+
+        InteractionInstructionsGO.SetActive(false);
+
+        prevDoorQuestionState = false;
         door.doorQuestion.showQuestion = true;
 
         door.ExternalEnterDoorInvokation(playerC.transform);
+    }
+
+    public void OnPlayerExit(Collider playerC)
+    {
+        if(!playerC.CompareTag("Player"))
+            return;
+
+        isTriggered = false;
+        InteractionInstructionsGO.SetActive(false);
     }
 
     public void OnDoorInteractionOver(Door door)
     {
         if(this.door == null || door.index != this.door.index)
             return;
-        
+            
         door.doorQuestion.showQuestion = prevDoorQuestionState;
         if(!door.enabled)
             this.door = null;
+            
+        if(this.door != null && isTriggered)
+            InteractionInstructionsGO.SetActive(true);
     }
 
     public void OnDoorInteractionStart(Door door)
     {
         if(door.doorQuestion.showQuestion || door.doorQuestion.mode == QuestionPresentationMode.Key)
             return;
+        
+        InteractionInstructionsGO.SetActive(false);
         
         this.door = door;
     }
